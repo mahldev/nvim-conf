fa072333941cc4891f3d8fa1fdcae924a5eadf45
mahl <robertolopezt75@gmail.com>
2023-12-06 14:05:49 +0100

    Update treesitter configuration and key mappings

--- lua/mahl/remap.lua
+++ lua/mahl/remap.lua
@@ -40,9 +40,9 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/mahl/packer.lua<CR>");
+vim.api.nvim_set_keymap("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/mahl/packer.lua<CR>", {noremap = true, silent = true});
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
